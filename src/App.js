caf967a095c0bd83cef79d07cfa801f8cfe48153
Justyna Liwak <justyna.liwak91@gmail.com>
2022-08-10 16:51:56 +0200

    setAllDone interaction

--- src/App.js
+++ src/App.js
@@ -30,6 +30,12 @@ function App() {
     }));
   };
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task, done: true
+    })));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -40,7 +46,7 @@ function App() {
       <Section
         title="Lista zadań"
         body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} setAllDone={setAllDone} />}
       />
     </Container>
   );
